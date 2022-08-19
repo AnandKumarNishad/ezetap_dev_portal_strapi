1f8438799ad136622b91c5a835f8ae606f337cb2
Anand Kumar Nishad <anandkumar.nishad25@.com>
2022-08-19 13:09:42 +0530

    initial commit

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,10 +1,8 @@
-// Path: ./config/env/production/server.js`
-
 module.exports = ({ env }) => ({
-  url: env('MY_HEROKU_URL'),
-  Proxy: true,
-  app: {
-    keys: env.array("APP_KEYS", ["2tdk4ZzUcWq1hV6cGSW9wg==", "K9Wps1ozLgtQzAEDFkJsQA=="]),
-  },
-});
-
+    url: env("MY_HEROKU_URL"),
+    proxy: true,
+    app: {
+      keys: env.array("APP_KEYS", ["testKey1", "testKey2"]),
+    },
+  });
+  
